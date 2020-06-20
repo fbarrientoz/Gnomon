347aa5dfec76cde72d22d76cb4bb7a761172b24e
Fernando Viscarra <fernandovs1@live.com>
2020-06-20 01:42:30 -0700

    se agrego la logica de escuchadores para la clase favoritos

--- app/src/main/java/com/uabc/edu/gnomon/ui/explorar/explorar.kt
+++ app/src/main/java/com/uabc/edu/gnomon/ui/explorar/explorar.kt
@@ -55,7 +55,8 @@ class explorar : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ExplorarViewModel::class.java)
+
+        //viewModel = ViewModelProviders.of(this).get(ExplorarViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
