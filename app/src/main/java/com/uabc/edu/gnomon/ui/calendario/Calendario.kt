1000b39d45ffda01fda33e3ee5aa15063afa36d4
Fernando Viscarra <fernandovs1@live.com>
2020-06-20 13:43:13 -0700

    Se creo el fragment de Calendario.kt y calendario_fragment.xml

--- app/src/main/java/com/uabc/edu/gnomon/ui/calendario/Calendario.kt
+++ app/src/main/java/com/uabc/edu/gnomon/ui/calendario/Calendario.kt
@@ -1,4 +1,4 @@
-package com.uabc.edu.gnomon.ui.perfil
+package com.uabc.edu.gnomon.ui.calendario
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
@@ -8,25 +8,25 @@ import android.view.View
 import android.view.ViewGroup
 import com.uabc.edu.gnomon.R
 
-class perfil : Fragment() {
+class Calendario : Fragment() {
 
     companion object {
-        fun newInstance() = perfil()
+        fun newInstance() = Calendario()
     }
 
-    private lateinit var viewModel: PerfilViewModel
+    private lateinit var viewModel: CalendarioViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.perfil_fragment, container, false)
-        return inflater.inflate(R.layout.perfil_fragment, container, false)
+        return inflater.inflate(R.layout.calendario_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PerfilViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(CalendarioViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
