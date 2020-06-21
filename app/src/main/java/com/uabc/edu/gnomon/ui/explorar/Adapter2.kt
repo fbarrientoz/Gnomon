70f11a2f7c37262bbd5192c101065534c1d75ca4
Fernando Viscarra <fernandovs1@live.com>
2020-06-20 17:26:02 -0700

    Modificaciòn de explorer para mostrar dos recycler view se agrego imagen y cambio de color de boton a Login Se cambio color de botón a registro

---
+++ app/src/main/java/com/uabc/edu/gnomon/ui/explorar/Adapter2.kt
@@ -0,0 +1,33 @@
+package com.uabc.edu.gnomon.ui.explorar
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.uabc.edu.gnomon.R
+
+
+class Adapter2 (private val mDataSet: Array<String>)
+    : RecyclerView.Adapter<Adapter2.ViewHolder>() {
+
+    // En este ejemplo cada elemento consta solo de un nombre
+    class ViewHolder(var textView: CardView) : RecyclerView.ViewHolder(textView)
+
+    // El layout manager invoca este método para renderizar cada elemento
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_text_view2, parent, false) as CardView
+
+        // Aquí podemos definir tamaños, márgenes, paddings, etc
+        return ViewHolder(v)
+    }
+
+    // Este método asigna valores para cada elemento de la lista
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    // Cantidad de elementos del RecyclerView
+    // Puede ser más complejo (por ejm, si implementamos filtros o búsquedas)
+    override fun getItemCount() = mDataSet.size
+}
